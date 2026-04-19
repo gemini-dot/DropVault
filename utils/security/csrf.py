c3fe7570cc12d899d659e7e0aa328689aad0c556
admin_sam <samvasang1192011@gmail.com>
2026-04-19 22:17:32 +0700

    add: debug mode

--- utils/security/csrf.py
+++ utils/security/csrf.py
@@ -1,6 +1,6 @@
 import secrets
 from flask import session, request, abort
-
+from os import getenv
 
 def generate_csrf_token():
     if "csrf_token" not in session:
@@ -9,6 +9,10 @@ def generate_csrf_token():
 
 
 def validate_csrf():
+
+    if getenv("FLASK_ENV") == "development":
+        return
+
     token = request.headers.get("X-CSRF-Token")
     session_token = session.get("csrf_token")
 
