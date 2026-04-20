0ed0f2fd2ca906cdc9bc408e90d15ba48afea197
admin_sam <samvasang1192011@gmail.com>
2026-04-20 22:40:12 +0700

    update: headers(fix requirements.txt)

--- middlewares/security_headers.py
+++ middlewares/security_headers.py
@@ -1,3 +1,11 @@
+"""
+This module sets security headers for the Flask application to enhance security against common web vulnerabilities.
+@license: Private / Internal Use Only
+@copyright: 2026 DropVault Team
+@created: 2026-04-17 2:00 AM
+@Author: CuSam
+"""
+
 def register_security_headers(app):
     @app.after_request
     def set_security_headers(response):
